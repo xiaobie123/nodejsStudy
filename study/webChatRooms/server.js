1419a4c6ad545f07ef892502ebfd002f4166eef4
biehengjie <biehengjie@vichain.com>
2018-05-13 00:40:48 +0800

    静态文件服务器

---
+++ study/webChatRooms/server.js
@@ -0,0 +1,68 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const mime = require('mime');
+
+const cache = {};
+
+function send404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('error 404: respurce not found.');
+    response.end();
+}
+function sendFile(response, filePath, fileContents) {
+    const type = mime.getType(path.basename(filePath));
+    response.writeHead(200, {'Content-Type': type });
+    response.end(fileContents.toString());
+}
+function serverStatic(response, cache, absPath) {
+    if(cache[absPath]) {
+        sendFile(response, cache, cache[absPath]);
+    } else {
+        fs.exists(absPath,(esists) => {
+            if(esists) {
+                fs.readFile(absPath, (err, data) => {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                }); 
+            } else {
+                send404(response);
+            }
+        })
+        // fs.open(absPath, 'wx', (err, fd) => {
+        //     if (err) {
+        //         if (err.code === 'ENOENT') {
+        //           send404(response);
+        //           return;
+        //         }
+        //         throw err;
+        //       }
+        //       fs.readFile(absPath, (err, data) => {
+        //         if (err) {
+        //             send404(response);
+        //         } else {
+        //             cache[absPath] = data;
+        //             sendFile(response, cache, data);
+        //         }
+        //     }); 
+        //   });
+    }
+}
+const server = http.createServer((request, response) => {
+    let PUBLIC_PATH = path.resolve(__dirname, './public');
+    if (request.url === '/') {
+        filePath = PUBLIC_PATH + 'index.html';
+    } else {
+        filePath = PUBLIC_PATH + request.url;
+    }
+    const absPath = filePath;
+    serverStatic(response, cache, absPath);
+});
+
+server.listen(3000, () => {
+    console.log('server listening on port 3000');
+})
